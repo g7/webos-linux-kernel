ae7a201c22d6c692f7de6da695f99049d23a7836
Eugenio Paolantonio (g7) <me@medesimo.eu>
2013-04-10 22:52:22 +0200

    Removed BFS; added ROW I/O scheduler thanks to the porting effort of Mazout360 (https://github.com/Mazout360/lge-kernel-gb/)

--- include/linux/elevator.h
+++ include/linux/elevator.h
@@ -17,6 +17,9 @@ typedef int (elevator_allow_merge_fn) (struct request_queue *, struct request *,
 typedef int (elevator_dispatch_fn) (struct request_queue *, int);
 
 typedef void (elevator_add_req_fn) (struct request_queue *, struct request *);
+typedef int (elevator_reinsert_req_fn) (struct request_queue *,
+    struct request *);
+typedef bool (elevator_is_urgent_fn) (struct request_queue *);
 typedef int (elevator_queue_empty_fn) (struct request_queue *);
 typedef struct request *(elevator_request_list_fn) (struct request_queue *, struct request *);
 typedef void (elevator_completed_req_fn) (struct request_queue *, struct request *);
@@ -39,6 +42,8 @@ struct elevator_ops
 
 	elevator_dispatch_fn *elevator_dispatch_fn;
 	elevator_add_req_fn *elevator_add_req_fn;
+    elevator_reinsert_req_fn *elevator_reinsert_req_fn;
+    elevator_is_urgent_fn *elevator_is_urgent_fn;
 	elevator_activate_req_fn *elevator_activate_req_fn;
 	elevator_deactivate_req_fn *elevator_deactivate_req_fn;
 
@@ -104,6 +109,7 @@ extern void elv_merge_requests(struct request_queue *, struct request *,
 			       struct request *);
 extern void elv_merged_request(struct request_queue *, struct request *, int);
 extern void elv_requeue_request(struct request_queue *, struct request *);
+extern int elv_reinsert_request(struct request_queue *, struct request *);
 extern int elv_queue_empty(struct request_queue *);
 extern struct request *elv_former_request(struct request_queue *, struct request *);
 extern struct request *elv_latter_request(struct request_queue *, struct request *);
